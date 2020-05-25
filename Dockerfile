e83bac84a352513b5b5089dd6426abb09cb33404
Ubuntu <cyberhome@cyberhome.nrdrio4c3niu3mxre25uead02d.bx.internal.cloudapp.net>
2020-05-25 00:18:57 +0000

    serv update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM python:3-alpine
+FROM python:3.7
 
 ADD . .
 
 RUN pip install pycryptodome
+
+EXPOSE 8080
 
 CMD [ "python", "./rsp.py" ]
--- rsp.py
+++ rsp.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from Cryptodome import Random
-from Cryptodome.Random import random
-from Cryptodome.Util.number import *
+import socketserver
+from Crypto import Random
+from Crypto.Random import random
+from Crypto.Util.number import *
 from Secret.flag import flag
 
 sbox = [221, 229, 120, 8, 119, 143, 33, 79, 22, 93, 239, 118, 130, 12, 63, 207, 90, 240, 199, 20, 181, 4, 139, 98, 78, 32, 94, 108, 100, 223, 1, 173, 220, 238, 217, 152, 62, 121, 117, 132, 2, 55, 125, 6, 34, 201, 254, 0, 228, 48, 250, 193, 147, 248, 89, 127, 174, 210, 57, 38, 216, 225, 43, 15, 142, 66, 70, 177, 237, 169, 67, 192, 30, 236, 131, 158, 136, 159, 9, 148, 103, 179, 141, 11, 46, 234, 36, 18, 191, 52, 231, 23, 88, 145, 101, 17, 74, 44, 122, 75, 235, 175, 54, 40, 27, 109, 73, 202, 129, 215, 83, 186, 7, 163, 29, 115, 243, 13, 105, 184, 68, 124, 189, 39, 140, 138, 165, 219, 161, 150, 59, 233, 208, 226, 176, 144, 113, 146, 19, 224, 111, 126, 222, 178, 47, 252, 99, 87, 134, 249, 69, 198, 164, 203, 194, 170, 26, 137, 204, 157, 180, 168, 162, 56, 81, 253, 213, 45, 21, 58, 24, 171, 37, 82, 53, 50, 84, 196, 232, 242, 244, 64, 80, 10, 114, 212, 187, 205, 28, 51, 182, 16, 107, 245, 211, 85, 92, 195, 5, 197, 200, 31, 183, 61, 123, 86, 167, 154, 41, 151, 35, 247, 246, 153, 95, 206, 149, 76, 112, 71, 230, 106, 188, 172, 241, 72, 156, 49, 14, 214, 155, 110, 102, 116, 128, 160, 135, 104, 77, 91, 190, 60, 42, 185, 96, 97, 251, 218, 133, 209, 65, 227, 3, 166, 255, 25]
@@ -74,22 +75,29 @@ def check_win(a, b):
             return False
     return False
 
-def main():
-    print("Beat me in Rock Paper Scissors 10 consecutive times to get the flag")
-    for i in range(10):
+def chal(req):
+    req.sendall(b"Beat me in Rock Paper Scissors 20 consecutive times to get the flag\n")
+    for i in range(20):
         secret, rps = gen_commitments()
         move = rps[0][0]
-        print("Possible commitments, the first one is my move:", " ".join(map(lambda s: s[1], rps)))
-        inp = input("Your move xD:")
+        req.sendall(b"Here are the possible commitments, the first one is my move:" + " ".join(map(lambda s: s[1], rps)).encode() + b'\n')
+        req.sendall(b"Your move: ")
+        inp = req.recv(256).strip().decode()
         res = check_win(move, inp)
-        print("My move was:", move, "Secret was:", secret)
+        req.sendall(b"My move was:" + move.encode() + b"Secret was:" + secret.encode() + b'\n')
         if not res:
-            print("You lose <3:")
-            main()
+            req.sendall(b"You lose!")
+            exit(0)
 
-    print("You win FML...")
-    print("Your reward is", flag)
+    req.sendall(b"You win\n")
+    req.sendall(b"Your reward is " + flag + b'\n')
     exit(0)
 
+class TaskHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        chal(self.request)
+
 if __name__ == '__main__':
-    main()
+    socketserver.ThreadingTCPServer.allow_reuse_address = True
+    server = socketserver.ThreadingTCPServer(('0.0.0.0', 8080), TaskHandler)
+    server.serve_forever()
